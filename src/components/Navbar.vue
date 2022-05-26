f37c0f0162ff22442a108181e173a8bbbfc687ee
duwi haryanto <haryanto@uii.ac.id>
2022-05-26 10:07:06 +0700

    main

---
+++ src/components/Navbar.vue
@@ -0,0 +1,55 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="/">Kulineran</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/">Home </router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/foods">Foodssss </router-link>
+                    </li>
+                </ul>
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/keranjang">Keranjang
+                            <b-icon-bag></b-icon-bag>
+                            <span class="badge badge-success ml-2">{{updatekeranjang ? updatekeranjang.length:keranjangs.length}}</span>
+                        </router-link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template> 
+<script>
+import axios from "axios"
+export default {
+    name: "Navbar",
+    data(){
+        return{
+            keranjangs:[]
+        }
+    },
+    props: ["updatekeranjang"],
+    methods: {
+        setKeranjang(data) {
+            this.keranjangs = data
+        },
+    },
+    mounted() {
+        axios.get('http://localhost:3000/keranjangs/')
+            .then((response) => this.setKeranjang(response.data))
+            .catch((error) => console.log("Gagal,", error));
+    }
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/Checkout.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <NavbarVue></NavbarVue>
+        <div class="container">
+            <div class="row justify-content-center mt-5">
+                <div class="col text-center">
+                    <img src="../assets/images/menunggu.png" width="300" />
+                    <h2><strong>Sukses!!</strong></h2>
+                    <h4>Pesanan Anda segera diproses <br> Selamat Menunggu</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import NavbarVue from '../components/Navbar.vue'
+export default {
+    name: 'Checkout',
+    components: {
+        NavbarVue
+    }
+}
+</script>
+
+<style>
+</style>
